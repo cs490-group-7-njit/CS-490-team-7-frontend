fb9f756e25b6acb29346c0eceac29b53e6b02369
YassinShater <shateryassin@gmail.com>
2025-11-04 14:46:34 -0500

    Implemented UC-2 Vendors feature: added VendorsPage, VendorFormPage, and updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import PrivateRoute from './components/PrivateRoute'
-import SmartHome from './components/SmartHome'
-import { AuthProvider } from './context/AuthContext'
-import LoginPage from './pages/LoginPage'
-import DashboardPage from './pages/DashboardPage'
-import StaffManagementPage from './pages/StaffManagementPage'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import PrivateRoute from './components/PrivateRoute';
+import SmartHome from './components/SmartHome';
+import { AuthProvider } from './context/AuthContext';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import StaffManagementPage from './pages/StaffManagementPage';
+import VendorsPage from "./pages/VendorsPage.jsx";
+import VendorFormPage from "./pages/VendorFormPage.jsx";
+
 
 function App() {
   return (
@@ -14,27 +17,48 @@ function App() {
         <Routes>
           <Route path="/" element={<SmartHome />} />
           <Route path="/login" element={<LoginPage />} />
+
           <Route
             path="/dashboard"
-            element={(
+            element={
               <PrivateRoute>
                 <DashboardPage />
               </PrivateRoute>
-            )}
+            }
           />
+
           <Route
             path="/staff"
-            element={(
+            element={
               <PrivateRoute>
                 <StaffManagementPage />
               </PrivateRoute>
-            )}
+            }
           />
+
+          {}
+          <Route
+            path="/vendors"
+            element={
+              <PrivateRoute>
+                <VendorsPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/vendors/new"
+            element={
+              <PrivateRoute>
+                <VendorFormPage />
+              </PrivateRoute>
+            }
+          />
+
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
